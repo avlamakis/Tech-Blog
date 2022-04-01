7f1576a65e17556c6502e652a3252bfc25964344
alexvlamakis <alexvlamakis@gmail.com>
2022-04-01 05:28:55 -0600

    reset port

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.set("port", PORT);
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
